c7d5d83230945a7d34953f862c0223138bba7902
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-06-02 12:12:28 +0300

    Delete unnecassary annotation.

--- src/main/java/com/egor/socialapi/repos/PostsRepo.java
+++ src/main/java/com/egor/socialapi/repos/PostsRepo.java
@@ -6,11 +6,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
-@Repository
 public interface PostsRepo extends CrudRepository<Posts, Long>, PagingAndSortingRepository<Posts, Long> {
     Page<Posts> findPostsByLoggedInUserIn(Collection<User> loggedInUser, Pageable pageable);
 }
