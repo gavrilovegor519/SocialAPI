64aa31bb8c25cb6272202023944f6312a509dca7
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-06-22 08:15:53 +0300

    Add start code for some controllers.

---
+++ src/main/java/com/egor/socialapi/controllers/FriendsController.java
@@ -0,0 +1,8 @@
+package com.egor.socialapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FriendsController {
+    //TODO: Make a controller for manage friends of user.
+}
---
+++ src/main/java/com/egor/socialapi/controllers/MessageController.java
@@ -0,0 +1,8 @@
+package com.egor.socialapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    //TODO: Make a controller for network's messages.
+}
